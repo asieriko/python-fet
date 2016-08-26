348456cd211ca870b7ef182f6e861f5e0ffe84ee
asieriko <asieriko@gmail.com>
2016-08-26 08:57:10 +0200

    testgroups generates both teachers and groups timetables odt file

--- ordutegia/testgroups.py
+++ ordutegia/testgroups.py
@@ -12,11 +12,13 @@ import xml.dom.minidom
 from datetime import datetime
 from itertools import chain
 from odf.opendocument import OpenDocumentText
-from odf.style import Style, TextProperties, ParagraphProperties,TableCellProperties
+from odf.style import Style, TextProperties, ParagraphProperties,TableCellProperties, GraphicProperties
+from odf.text import P, H, List, ListItem
+from odf.table import Table, TableColumn, TableRow, TableCell
 from odf import table, text
 
-fileg = "/home/asier/Hezkuntza/python-hezkuntza/python-fet/EDUCA/subgroups.xml"
-filet = "/home/asier/Hezkuntza/python-hezkuntza/python-fet/EDUCA/teachers.xml"
+fileg = "/home/asier/Hezkuntza/python-hezkuntza/python-fet/ordutegia/subgroups.xml"
+filet = "/home/asier/Hezkuntza/python-hezkuntza/python-fet/ordutegia/teachers.xml"
 
 tree = ET.parse(fileg)     
 root = tree.getroot()
@@ -25,27 +27,45 @@ tree2 = ET.parse(filet)
 root2 = tree2.getroot()
 
 orduak = ['08:30-9:25','09:25-10:20','10:20-11:15', '11:15-11:45','11:45-12:40','12:40-13:35', '13:35-14:30','14:30-15:20']
-textdoc = OpenDocumentText()
-#def textdoc_init():
+
+
+h1style = Style(name="Heading 1",  family="paragraph",parentstylename="Heading 1")
+h1style.addElement(GraphicProperties(fill="solid",fillcolor="#e6e6ff"))
+h1style.addElement(TextProperties(attributes={'fontsize':"14pt",'fontweight':"bold",'color':"#000099" }))
+h1style.addElement(ParagraphProperties(breakbefore="page",margintop="0.4cm",marginbottom="0.2cm",backgroundcolor="#e6e6ff",padding="0.05cm",borderleft="none",borderright="none",bordertop="none",borderbottom="2.01pt solid #000099",shadow="none"))
+
 # Create a style for the paragraph with page-break
 withbreak = Style(name="WithBreak", parentstylename="Standard", family="paragraph")
 withbreak.addElement(ParagraphProperties(breakbefore="page"))
-textdoc.automaticstyles.addElement(withbreak)
 
 TAB_style = Style(name="Table", family="table-cell")
 TAB_style.addElement(TableCellProperties(border="0.05pt solid #000000"))
-textdoc.automaticstyles.addElement(TAB_style)
 
-h1style = Style(name="Heading 1", family="paragraph")
-h1style.addElement(TextProperties(attributes={'fontsize':"24pt",'fontweight':"bold" }))
-h1style.addElement(ParagraphProperties(breakbefore="page"))
-textdoc.automaticstyles.addElement(h1style)
+tableheaders = Style(name="Table Headers", family="paragraph", parentstylename="Standard")
+tableheaders.addElement(ParagraphProperties(numberlines="false", linenumber="0",textalign="center",margintop="0.2cm",marginbottom="0.2cm"))
+tableheaders.addElement(TextProperties(attributes={'fontsize':"12pt",'fontweight':"bold"}))
 
-p = text.P(text=u'Horarios por grupos')
-textdoc.text.addElement(p)
+def createdoc():
+    
+    textdoc = OpenDocumentText()
+    #def textdoc_init():
+    
+    textdoc.automaticstyles.addElement(withbreak)
+
+    
+    textdoc.automaticstyles.addElement(TAB_style)
+    
+    
+    textdoc.styles.addElement(tableheaders)
+
+    
+    textdoc.automaticstyles.addElement(h1style)
+    
+    return textdoc
 
 
-def print_odf(Matrix,name):
+
+def print_odf(Matrix,name,textdoc,odtfile):    
     hours = len(Matrix)
     h=text.H(outlinelevel=1, stylename=h1style, text=name)
     textdoc.text.addElement(h)
@@ -58,9 +78,10 @@ def print_odf(Matrix,name):
     tr = table.TableRow()
     t.addElement(tr)
     for eguna in ('Saioa','Astelehena','Asteartea','Asteazkena','Osteguna','Ostirala'):
-        tc = table.TableCell(valuetype="string", stylename="Table")
-        tc.addElement(text.P(text=eguna))
+        tc = TableCell(stylename="Table")
         tr.addElement(tc)
+        p = P(stylename=tableheaders,text=eguna)
+        tc.addElement(p)
     for hour in range(hours):
         if hour == 3:
             tr = table.TableRow()
@@ -78,10 +99,14 @@ def print_odf(Matrix,name):
     textdoc.text.addElement(datatable)
     
 
-    textdoc.save("ordutegia_ikasle.odt")
+    textdoc.save(odtfile)
 
 
 def findsg(groups,verbose=False):
+    textdoc = createdoc()            
+    p = text.P(text=u'Horarios por grupos')
+    textdoc.text.addElement(p)
+    
     for group in groups:
         subgroups = root.xpath(".//Subgroup[starts-with(@name,'"+group+"')]")
         w, h = 5, 8 
@@ -108,40 +133,44 @@ def findsg(groups,verbose=False):
         else:
             dbh = False
         printmat(Matrix,verbose)
-        print_odf(Matrix,group)
+        print_odf(Matrix,group,textdoc,"ordutegia_ikasle.odt")
 
 
 def findt():
-        teachers = root2.xpath(".//Teacher")
-        w1, h1 = 5, 8 
-        #Matrix = [[[] for x in range(w)] for y in range(h)]
-        for s in teachers: 
-            group = s.attrib.get('name')
-            #print(s.attrib['name'])
-            ds = s.findall(".//Day")
-            i = 0
-            Matrix = [[[] for x in range(w1)] for y in range(h1)]
-            for d in ds:
-                hs = d.findall(".//Hour")
-                j = 0
-                for h in hs:
-                    sub = h.findall(".//Subject")
-                    stud = h.findall(".//Students")
-                    stu = []
-                    for st in stud:
-                        stu.append(st.attrib.get('name'))
-                    if sub != [] and Matrix[j][i].count(sub[0].attrib['name'])==0:
-                        #print(d.attrib['name'],h.attrib['name'],sub[0].attrib['name'])
-                        #print(i,j)
-                        if stu != '':
-                            text = sub[0].attrib['name'] + ' (' + '-'.join(stu) + ')'
-                        else:
-                            text = sub[0].attrib['name']
-                        Matrix[j][i].append(text)
-                    j += 1
-                i += 1
-            print(group)
-            print_odf(Matrix,group)
+    teachers = root2.xpath(".//Teacher")
+    textdoc = createdoc()       
+    #p = text.P(text=u'Horarios por profesores')
+    #textdoc.text.addElement(p)
+    
+    w1, h1 = 5, 8 
+    #Matrix = [[[] for x in range(w)] for y in range(h)]
+    for s in teachers: 
+        group = s.attrib.get('name')
+        #print(s.attrib['name'])
+        ds = s.findall(".//Day")
+        i = 0
+        Matrix = [[[] for x in range(w1)] for y in range(h1)]
+        for d in ds:
+            hs = d.findall(".//Hour")
+            j = 0
+            for h in hs:
+                sub = h.findall(".//Subject")
+                stud = h.findall(".//Students")
+                stu = []
+                for st in stud:
+                    stu.append(st.attrib.get('name'))
+                if sub != [] and Matrix[j][i].count(sub[0].attrib['name'])==0:
+                    #print(d.attrib['name'],h.attrib['name'],sub[0].attrib['name'])
+                    #print(i,j)
+                    if stu != '':
+                        text = sub[0].attrib['name'] + ' (' + '-'.join(stu) + ')'
+                    else:
+                        text = sub[0].attrib['name']
+                    Matrix[j][i].append(text)
+                j += 1
+            i += 1
+        print(group)
+        print_odf(Matrix,group,textdoc,"ordutegia_irakasle.odt")
 
 
 def printmat(mat,verbose=False):
@@ -157,5 +186,5 @@ def printmat(mat,verbose=False):
 
 groups = ['1-A','1-B','1-C','1-D','1-E','1-H','1-I','1-J','1-K','1-L','2-A','2-B','2-C','2-D','2-P','2-H','2-I','2-J','3-A','3-B','3-C','3-P','3-H','3-I','3-J','3-K','3-Q','4-A','4-B','4-C','4-D','4-H','4-I','4-J','4-K','4-L','5-A','5-B','5-H','5-I','5-J','6-A','6-B','6-H','6-I','6-J']
 #textdoc_init()
-findsg(groups)
-#findt()
+#findsg(groups)
+findt()
